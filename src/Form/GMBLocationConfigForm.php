991f07a622e96621e2dcae364ad4787c937ff17d
pcs-anthonyjohnson <anthonyjohnson@parmentercreeksoftware.com>
2021-09-30 11:45:38 -0600

    Location Config Form Submit
    The Location Config Form now saves location name, and star min values.
    The form also queries the GMB API for the location name listed and, if
    found, populates the Location ID setting.

--- src/Form/GMBLocationConfigForm.php
+++ src/Form/GMBLocationConfigForm.php
@@ -68,6 +68,87 @@ class GMBLocationConfigForm extends FormBase {
 
   public function submitForm(array &$form, FormStateInterface $formState) {
 
+    $settings = $formState->getValues()['settings_container'];
+
+    // Load stored config
+    $config = \Drupal::service('config.factory')
+      ->getEditable('google_reviews_testimonials.settings');
+    $accountID = $config->get('accountID');
+    $locationName = $config->get('locationName');
+    $starMin = $config->get('starMin');
+    $serviceKey = $config->get('serviceKey');
+    $subject = $config->get('subject');
+    $scopes = $config->get('scopes');
+
+    // Update the location name and star min, if necessary.
+    if($locationName != $settings['location_name']) {
+
+      $config->set('locationName', $settings['location_name'])->save();
+      $locationName = $settings['location_name'];
+    }
+
+    if($starMin != $settings['star_min']) {
+
+      $config->set('starMin', $settings['star_min'])->save();
+      $starMin = $settings['star_min'];
+    }
+
+    // Authenticate and query for the location
+    $queryURL = 'https://mybusiness.googleapis.com/v4/accounts/' 
+      . $accountID 
+      . '/locations';
+    $tmpSK = (array)json_decode($serviceKey);
+    $googleClient = new \Google\Client();
+    $googleClient->setAuthConfig($tmpSK);
+    $googleClient->setScopes($scopes);
+    $googleClient->setSubject($subject);
+    $httpClient = $googleClient->authorize();
+    $response = json_decode(strval($httpClient
+      ->get($queryURL)
+      ->getBody()));
+    
+    if($response->locations) {
+
+      $locationSet = FALSE;
+
+      // Find the location with an exact match on the locationName property.
+      // Note: locationName and name are different values returned from the
+      // API!
+      foreach($response->locations as $location) {
+        
+        if($location->locationName == $locationName) {
+
+          // Once again, the location ID required for use in the API is not
+          // provided in a straight-forward manner. The locationName property,
+          // which contains the location ID, has this value in the following
+          // format: 
+          // accounts/[accountID]/locations/[locationID]
+          // * Square brackets not included
+          // Therefore, we need this explode to strip out all the unnecessary
+          // info and get the good stuff.
+          $locationID = explode('/', $location->name)[3];
+          $config->set('locationID', $locationID)->save();
+
+          \Drupal::messenger()->addMessage('Location saved.');
+          $locationSet = true;
+          break;
+        }
+      }
+
+      if(!$locationSet) {
+
+        $config->set('locationID', '')->save();
+        \Drupal::messenger()->addError('No location found with that name.');
+
+      }
+    }
+    else {
+      
+      $message = 'An error occured while querying the Google API. Did you '
+        . 'setup your Account ID first? Visit the README for more details.';
+      \Drupal::messenger()->addError($message);
+
+    }
   }
 
 }
