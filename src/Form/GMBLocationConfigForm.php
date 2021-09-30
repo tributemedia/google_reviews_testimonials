10c8ae53f43b5ac20bc7d92f9a1033b7c1989d36
pcs-anthonyjohnson <anthonyjohnson@parmentercreeksoftware.com>
2021-09-30 10:53:13 -0600

    Form Structure of GMB Location Config
    Completed the form structure for GMB Location Config. All that's missing
    is submit logic.

--- src/Form/GMBLocationConfigForm.php
+++ src/Form/GMBLocationConfigForm.php
@@ -18,11 +18,49 @@ class GMBLocationConfigForm extends FormBase {
 
   public function buildForm(array $form, 
     FormStateInterface $formState = NULL) {
+
+    // Load default config values
+    $config = \Drupal::config('google_reviews_testimonials.settings');
+    $locationID = $config->get('locationID');
+    $locationName = $config->get('locationName');
+    $starMin = $config->get('starMin');
     
+    // Build the form
     $form = [];
 
-    $form['test'] = array(
-      '#markup' => 'Hello form!',
+    $form['settings_container'] = array(
+      '#type' => 'container',
+      '#attributes' => array(
+        'class' => 'settings-container',
+      ),
+      '#tree' => TRUE,
+    );
+
+    $form['settings_container']['location_id'] = array(
+      '#type' => 'textfield',
+      '#title' => 'Location ID',
+      '#default_value' => $locationID,
+      '#disabled' => TRUE,
+    );
+
+    $form['settings_container']['location_name'] = array(
+      '#type' => 'textfield',
+      '#title' => 'Location Name',
+      '#default_value' => $locationName,
+      '#required' => TRUE,
+    );
+
+    $form['settings_container']['star_min'] = array(
+      '#type' => 'number',
+      '#title' => 'Review Star Minimum',
+      '#default_value' => $starMin,
+      '#min' => 1,
+      '#max' => 5,
+    );
+
+    $form['save'] = array(
+      '#type' => 'submit',
+      '#value' => 'Save',
     );
 
     return $form;
